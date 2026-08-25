94a04af9d7345716f4974d588b15742d4df41ad9
Fady Nagh <Fady@Fadyio.com>
2026-08-25 13:33:47 +0300

    feat(infra): route app.croviq.app to Google load balancer

--- infra/cloudflare-dns/variables.tf
+++ infra/cloudflare-dns/variables.tf
@@ -21,3 +21,9 @@ variable "certificate_dns_authorization_value" {
   description = "The DNS Resource Record Value (data) for Google Certificate Manager DNS authorization."
   default     = "37a12037-33a6-40bc-9905-b7e8d0287946.12.authorize.certificatemanager.goog."
 }
+
+variable "app_ipv4_address" {
+  type        = string
+  description = "The IPv4 address of the Google Cloud Load Balancer for app.croviq.app."
+  default     = "8.233.204.233"
+}
